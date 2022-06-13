a3eea1114cd79836cae7a30cf31c709d4cc05041
alisson <alisson.correa@climba.com.br>
2022-06-13 09:56:23 -0300

    Adicionado no status o status partially_refunded
    Adicionado no status o status partially_refunded pois para poder fazer o estorno parcial e poder validar precisamos comparar o status que está vindo no webservide

--- src/Iugu/Enum/InvoiceStatusEnum.php
+++ src/Iugu/Enum/InvoiceStatusEnum.php
@@ -14,20 +14,22 @@ class InvoiceStatusEnum
     const DONE                    = 'done';
     const PARTIALLY_PAID          = 'partially_paid';
     const IN_PROTEST              = 'in_protest';
+    const PARTIALLY_REFUNDED      = 'partially_refunded';
     
     public static function getDescription($statusCode){
         
         switch($statusCode) {
-            case static::PENDING:           return 'Pendente';
-            case static::REFUNDED:          return 'Reembolsado';
-            case static::CANCELED:          return 'Cancelado';
-            case static::EXPIRED:           return 'Expirado';
-            case static::AUTHORIZED:        return 'Autorizado';
-            case static::IN_ANALYSIS:       return 'Em Análise';
-            case static::PAID:              return 'Pago';
-            case static::DONE:              return 'Concluído';
-            case static::PARTIALLY_PAID:    return 'Pago Parcialmente';
-            case static::IN_PROTEST:        return 'Em Protesto';
+            case static::PENDING:                   return 'Pendente';
+            case static::REFUNDED:                  return 'Reembolsado';
+            case static::CANCELED:                  return 'Cancelado';
+            case static::EXPIRED:                   return 'Expirado';
+            case static::AUTHORIZED:                return 'Autorizado';
+            case static::IN_ANALYSIS:               return 'Em Análise';
+            case static::PAID:                      return 'Pago';
+            case static::DONE:                      return 'Concluído';
+            case static::PARTIALLY_PAID:            return 'Pago Parcialmente';
+            case static::IN_PROTEST:                return 'Em Protesto';
+            case static::PARTIALLY_REFUNDED:        return 'Estornado Parcialmente';
         }
     
         return 'Desconhecido';
